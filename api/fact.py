31b055e48208dc28129013184556536a014e1efa
EshikaP1 <fairspringgal@gmail.com>
2023-10-19 15:39:28 -0700

    fact 1

--- api/fact.py
+++ api/fact.py
@@ -1,99 +1,30 @@
-from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
-from flask_restful import Api, Resource # used for REST API building
-import requests  # used for testing 
-import random
+import requests
 
-from model.jokes import *
+# Define your API key
+api_key = "pqlcywj9X23Yr9Xls0VhUaX4SOaHqAwJ8X3bjUlD"
 
-joke_api = Blueprint('joke_api', __name__,
-                   url_prefix='/api/jokes')
+# ...
 
-# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
-api = Api(joke_api)
+# get count of jokes on server
+count_response = requests.get("https://api.cancer.gov" + "/count", headers={"Authorization": f"Bearer {api_key}"})
+count_json = count_response.json()
+count = count_json['count']
 
-class JokesAPI:
-    # not implemented
-    class _Create(Resource):
-        def post(self, joke):
-            pass
-            
-    # getJokes()
-    class _Read(Resource):
-        def get(self):
-            return jsonify(getJokes())
+# update likes/dislikes test sequence
+num = str(random.randint(0, count - 1))  # test a random record
+responses.append(
+    requests.get("https://api.cancer.gov" + "/" + num, headers={"Authorization": f"Bearer {api_key}"})  # read joke by id
+)
+responses.append(
+    requests.put("https://api.cancer.gov" + "/like/" + num, headers={"Authorization": f"Bearer {api_key}"})  # add to like count
+)
+responses.append(
+    requests.put("https://api.cancer.gov" + "/jeer/" + num, headers={"Authorization": f"Bearer {api_key}"})  # add to jeer count
+)
 
-    # getJoke(id)
-    class _ReadID(Resource):
-        def get(self, id):
-            return jsonify(getJoke(id))
+# obtain a random joke
+responses.append(
+    requests.get("https://api.cancer.gov" + "/random", headers={"Authorization": f"Bearer {api_key}"})  # read a random joke
+)
 
-    # getRandomJoke()
-    class _ReadRandom(Resource):
-        def get(self):
-            return jsonify(getRandomJoke())
-    
-    # getRandomJoke()
-    class _ReadCount(Resource):
-        def get(self):
-            count = countJokes()
-            countMsg = {'count': count}
-            return jsonify(countMsg)
-
-    # put method: addJokeHaHa
-    class _UpdateLike(Resource):
-        def put(self, id):
-            addJokeHaHa(id)
-            return jsonify(getJoke(id))
-
-    # put method: addJokeBooHoo
-    class _UpdateJeer(Resource):
-        def put(self, id):
-            addJokeBooHoo(id)
-            return jsonify(getJoke(id))
-
-    # building RESTapi resources/interfaces, these routes are added to Web Server
-    api.add_resource(_Create, '/create/<string:joke>')
-    api.add_resource(_Read, '/')
-    api.add_resource(_ReadID, '/<int:id>')
-    api.add_resource(_ReadRandom, '/random')
-    api.add_resource(_ReadCount, '/count')
-    api.add_resource(_UpdateLike, '/like/<int:id>')
-    api.add_resource(_UpdateJeer, '/jeer/<int:id>')
-    
-if __name__ == "__main__": 
-    # server = "http://127.0.0.1:5000" # run local
-    server = 'https://flask.nighthawkcodingsociety.com' # run from web
-    url = server + "/api/jokes"
-    responses = []  # responses list
-
-    # get count of jokes on server
-    count_response = requests.get(url+"/count")
-    count_json = count_response.json()
-    count = count_json['count']
-
-    # update likes/dislikes test sequence
-    num = str(random.randint(0, count-1)) # test a random record
-    responses.append(
-        requests.get(url+"/"+num)  # read joke by id
-        ) 
-    responses.append(
-        requests.put(url+"/like/"+num) # add to like count
-        ) 
-    responses.append(
-        requests.put(url+"/jeer/"+num) # add to jeer count
-        ) 
-
-    # obtain a random joke
-    responses.append(
-        requests.get(url+"/random")  # read a random joke
-        ) 
-
-    # cycle through responses
-    for response in responses:
-        print(response)
-        try:
-            print(response.json())
-        except:
-            print("unknown error")
-            
-            #hello
+# ...
