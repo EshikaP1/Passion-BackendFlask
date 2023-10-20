7cb285f12a64ad129bb6f1c62dd8d68190adef93
samhita-l <samhitalagisetti@gmail.com>
2023-10-20 22:27:32 +0000

    hi

--- api/fact1.py
+++ api/fact1.py
@@ -1,15 +1,24 @@
 from flask import Flask, jsonify
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
 # Load data from the CSV file
-data = pd.read_csv('cancer.csv')
+csv_file_path = os.environ.get("CSV_FILE_PATH", "cancer.csv")
 
-# Define an API endpoint to retrieve data``
+try:
+    data = pd.read_csv(csv_file_path)
+except FileNotFoundError:
+    data = pd.DataFrame()  # Handle the case where the file is not found
+
+# Define an API endpoint to retrieve data
 @app.route('/api/data', methods=['GET'])
 def get_data():
+    if data.empty:
+        return jsonify({"error": "Data not available"}), 404
     return jsonify(data.to_dict(orient='records'))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
