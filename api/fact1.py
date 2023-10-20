94fcfd6fb2ea7b92cadb22dfd23028392fe9f1d9
cliang1 <c85liang@gmail.com>
2023-10-20 15:16:02 -0700

    backend

---
+++ api/fact1.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load data from the CSV file
+data = pd.read_csv('cancer.csv')
+
+# Define an API endpoint to retrieve data
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify(data.to_dict(orient='records'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
