0f01a9380c0bc15d2540f23fa09c644a332f7dfe
EshikaP1 <fairspringgal@gmail.com>
2023-10-23 11:46:41 -0700

    fact1 data

--- api/fact1.py
+++ api/fact1.py
@@ -4,12 +4,12 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load data from the CSV file
-data = pd.read_csv('cancer.csv')
+data = pd.read_csv("/home/eneter/vscode/Passion-BackendFlask/api/cancer.csv")
 
-# Define an API endpoint to retrieve data``
+# Define an API endpoint to retrieve data
 @app.route('/api/data', methods=['GET'])
 def get_data():
     return jsonify(data.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
