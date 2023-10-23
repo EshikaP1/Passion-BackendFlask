cd60d182c6fa34fb7ea4e4e75ee084fb235e1da5
EshikaP1 <fairspringgal@gmail.com>
2023-10-23 12:53:04 -0700

    commit trial

--- api/fact1.py
+++ api/fact1.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pandas as pd
 
 app = Flask(__name__)
 
 # Load data from the CSV file
 data = pd.read_csv("/home/eneter/vscode/Passion-BackendFlask/api/cancer.csv")
+#get the input from frontend
+data = request.get_json()
+state_name = data.get('state_name', '')
 
+column_data = data[state_name]
 # Define an API endpoint to retrieve data
 @app.route('/api/data', methods=['GET'])
 def get_data():
@@ -13,3 +17,4 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
