2ed8b57eee6a93e7401e50ffab3fdc502d9a08b1
cliang1 <c85liang@gmail.com>
2023-10-25 15:36:03 -0700

    comments

--- api/fact1.py
+++ api/fact1.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify, request
 import pandas as pd
 import os
 
+# class provided by the Flask framework for creating web applications
 app = Flask(__name__)
 
 # Get the directory of your Flask app's root
+# stores the root directory path of your Flask application.
+# This can be useful for various purposes, such as locating templates, static files, or other resources relative to your application's root directory
+
 app_root = os.path.dirname(os.path.abspath(__file__))
 
 # Create the full path to your CSV file
@@ -14,6 +18,8 @@ csv_path = os.path.join(app_root, 'cancer.csv')
 data = pd.read_csv(csv_path)
 
 # Define an API endpoint to retrieve data
+#when our user promts data, it will redirect here instead of another file.
+
 @app.route('/api/data', methods=['GET'])
 def get_data(): 
     if data.empty:
@@ -23,6 +29,7 @@ def get_data():
 # Define an API endpoint to receive state_name from the frontend
 @app.route('/backend_endpoint', methods=['POST'])
 def process_state_input():
+    #since post was used to get data from the frontend, get_json wil retrieve data from the
     state_data = request.get_json()
     state_name = state_data.get('state_name', '')
 
