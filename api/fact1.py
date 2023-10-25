f47bd83712c896aac3ae6a436420c111e4183fe8
cliang1 <c85liang@gmail.com>
2023-10-25 15:34:01 -0700

    fact debugging

--- api/fact1.py
+++ api/fact1.py
@@ -1,36 +1,28 @@
 from flask import Flask, jsonify, request
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
-# Load data from the CSV file
-data = pd.read_csv("api/cancer.csv")
-
-import os
-
 # Get the directory of your Flask app's root
-app_root = os.path.dirname(os.path.abspath(__file__))  # Assuming this code is in your Flask app file
+app_root = os.path.dirname(os.path.abspath(__file__))
 
 # Create the full path to your CSV file
-csv_path = os.path.join(app_root, 'api', 'cancer.csv')
+csv_path = os.path.join(app_root, 'cancer.csv')
 
 # Load data from the CSV file
 data = pd.read_csv(csv_path)
 
-
 # Define an API endpoint to retrieve data
-#when our user promts data, it will redirect here instead of another file. 
 @app.route('/api/data', methods=['GET'])
-#instead of having a separate file with endpoints, it is creating an enpoint here by using data as a variable
-def get_data():
+def get_data(): 
     if data.empty:
         return jsonify({"error": "Data not available"}), 404
     return jsonify(data.to_dict(orient='records'))
 
 # Define an API endpoint to receive state_name from the frontend
-@app.route('/backend_endpoint', methods=['POST'])  # Use POST to receive data from the frontend
+@app.route('/backend_endpoint', methods=['POST'])
 def process_state_input():
-    #since post was used to get data from the frontend, get_json wil retrieve data from the 
     state_data = request.get_json()
     state_name = state_data.get('state_name', '')
 
@@ -38,7 +30,7 @@ def process_state_input():
     # You can use it to filter the data or perform any other task
 
     # In this example, we'll return a message with the state_name
-    return jsonify( "Received state_name:" + (state_name))
+    return jsonify({"message": "Received state_name: " + state_name})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8115, debug=True)
