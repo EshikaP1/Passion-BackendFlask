e73ae50581dcf2c3fbf5eba46b0fa330d53fd283
EshikaP1 <fairspringgal@gmail.com>
2023-10-30 13:19:47 -0700

    commit23

--- api/fact3.py
+++ api/fact3.py
@@ -17,6 +17,7 @@ class CancerDataAPI:
             #determine the directory path of the current Python script or module in which it is placed. 
             #abspath: the absolute path of the current script file. 
             # extract the directory (folder) path from a given file path.
+            # using this code enhances portability, maintainability, and collaboration while reducing the risk of errors and security vulnerabilities.
             app_root = os.path.dirname(os.path.abspath(__file__))
             csv_path = os.path.join(app_root, 'cancer.csv')
             data = pd.read_csv(csv_path)
@@ -26,7 +27,7 @@ class CancerDataAPI:
 
     class _Create(Resource):
         def post(self):
-            # Here, you can handle the creation of a new entry if needed
+            # Here, you can handle the creation of a new entry if needed...this is because we are getting the input from the user(frontend) which is why post is called here and in frontend.
             #create or add new data entries to a resource, such as creating a new object or record in a database.
             # This might involve parsing request data, validating it, and then interacting with a database or performing other relevant operations.
             pass
@@ -35,7 +36,9 @@ class CancerDataAPI:
         def get(self, state_name):
             app_root = os.path.dirname(os.path.abspath(__file__))
             csv_path = os.path.join(app_root, 'cancer.csv')
+            #reat and get access to the data
             data = pd.read_csv(csv_path)
+            #its asking for
             state_data = data[data['State'] == state_name]
             if state_data.empty:
                 return jsonify({"error": "State data not found"}), 404
@@ -45,8 +48,11 @@ class CancerDataAPI:
             }
             return jsonify(result)
 
+#This line is defining a route at the root of the API
+#This route is typically used to handle POST and GEt requests for creating new data entries.
 api.add_resource(CancerDataAPI._Read, '/')
 api.add_resource(CancerDataAPI._Create, '/create')
+# This dynamic part allows for variable values to be included in the URL
 api.add_resource(CancerDataAPI._StateData, '/state/<string:state_name>')
 
 # Register the blueprint with the Flask app
